5ddc144561d0662f28dcecf90194235d6dd26635
Otabek Makhmudov <mahmudovotabek2208@gmail.com>
2025-07-16 18:25:03 +0500

    Statistics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import HomeComponent from "@/components/HomeComponent";
+import Statistics from "@/components/Statiscs";
 
 export default function Home() {
   return (
     <section className="container mx-auto p-6">
       <Header />
       <HomeComponent />
+      <Statistics />
     </section>
   );
 }
