738e56d77232be9b8ebaba32e824c4634d0f6389
Otabek Makhmudov <mahmudovotabek2208@gmail.com>
2025-07-17 14:59:18 +0500

    home responsive

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -4,7 +4,7 @@ import WomanSvg from "../../public/woman.svg";
 
 export default function HomeComponent() {
   return (
-    <section className="container mx-auto p-6 grid grid-cols-2 items-center min-h-96  border-solid border-b-2 border-[#5B666F40]">
+    <section className="container mx-auto p-6 pb-0 grid grid-cols-2 gap-8 items-center min-h-96  border-solid border-b-2 border-[#5B666F40]">
       <div className="col-span-2 md:col-span-1 my-8">
         <p className="text-5xl">Equity Financing</p>
         <p className="text-5xl">for startup employees</p>
@@ -21,7 +21,7 @@ export default function HomeComponent() {
           <Image src={WomanSvg} alt="woman" width={350} height={400} />
         </div>
 
-        <div className="absolute top-1/4 left-0 w-fit bg-[#fffbeb] border-solid border border-gray-300 rounded-xl ">
+        <div className="absolute bottom-0.5 right-0 md:left-0 md:top-1/4 w-fit bg-[#fffbeb] border-solid border border-gray-300 rounded-xl ">
           <p className="py-4 px-3 mb-3 border-solid border border-transparent border-b-gray-300">
             Exercise details
           </p>
